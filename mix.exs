110d706d8f73a7bb69a26c8ec24613c08955acf1
Haris Botić <haris.botic1@gmail.com>
2022-11-28 13:02:31 +0100

    Add Dialyxir dependency

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Game2048.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
     ]
   end
 
